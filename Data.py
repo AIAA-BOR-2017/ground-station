d88af1d23ce5d75855b92f70118372684d4c8294
NathanSiegel <siegelnathan08@gmail.com>
2017-03-19 16:05:32 -0400

    added imagedata and other stuff
    need to fix adddata

--- Data.py
+++ Data.py
@@ -1,9 +1,14 @@
 """Data structures for Ground Station"""
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
 class SensorFrame:
     'one frame of sensor data'
 
-    def __init__(self, time, batV, pres, temp, hum, gps):
+    def __init__(self, num, time, batV, pres, temp, hum, gps):
+        self.packetnum = num
         self.timeSeconds = time
         self.batVoltage = batV
         self.pressure = pres
@@ -47,19 +52,44 @@ class ImageFrame:
 
     def __init__(self):
         self.data = []
-        data = [[[0 for x in range(self.colors)] for y in range(self.width)] for z in range(self.height)]
+        self.data = [[[0 for x in range(self.colors)] for y in range(self.width)] for z in range(self.height)]
 
     def addData(self, packetNum, listPixels):
         'adds one packet of image data'
         #70 pixels per packet
-        xLoc = (packetNum*listPixels.len())%width
-        yLoc = int((packetNum*listPixels.len())/width)
+        width = 640
+        xLoc = (packetNum*35)% width
+        yLoc = int((packetNum*35)/width)
 
-        for i in range(listPixels.len()):
+        for i in range(len(listPixels)):
             xPix = xLoc + i
             yPix = yLoc
             if (xPix) >= width:
                 xPix -= width
                 yPix += 1
             self.data[xPix][yPix] = listPixels[i]
+        if packetNum == 8777:
+            imd.addImage(self)
         
+class ImageData:
+    def __init__(self):
+        self.images = []
+
+    def addImage(self, img):
+        'adds an image'
+        self.images.append(img)
+
+    def getImage(self, i):
+        'returns the selected image'
+        return self.images[i]
+    
+if __name__ == "__main__":
+    imf = ImageFrame()
+    imd = ImageData()
+    #imd.addImage(imf)
+    imf.addData(8777, [[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255],[255,255,255]])
+    implt = np.asarray(imd.getImage(0).data, dtype = np.uint8)
+    plt.axis("off")
+    plt.imshow(implt)
+    plt.show()
+
