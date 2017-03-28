c5b3a2e59220fa818ca7418b4d5204d7b42217ae
NathanSiegel <siegelnathan08@gmail.com>
2017-03-28 19:00:36 -0400

    work on telemetry packets & packet processing
    a

--- PacketParsing.py
+++ PacketParsing.py
@@ -10,4 +10,14 @@ def isTelemetry(packet):
     print(packet[0])
         
 def telemetryDataAssembler(packet):
-    
+    batV = packet[0]<<8 + packet[1]
+    print(batV)
+    print(packet[0])
+    print(packet[1])
+
+def receivePacket(rawPacket):
+    'converts string packet to a list of 8-bit ints'
+    packet = []
+    for elt in rawPacket:
+        packet.append(ord(elt))
+    return packet
