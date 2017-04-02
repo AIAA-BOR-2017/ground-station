1163e983ab09d7453b071626e19799fb5acbeec9
NathanSiegel <siegelnathan08@gmail.com>
2017-04-02 15:20:18 -0400

    improved telemetry parsing, added image parsing
    made sure similar functions were going to be used in more similar ways

--- Data.py
+++ Data.py
@@ -7,14 +7,15 @@ import numpy as np
 class SensorFrame:
     'one frame of sensor data'
 
-    def __init__(self, num, time, batV, pres, temp, hum, gps):
-        self.packetnum = num
-        self.timeSeconds = time
-        self.batVoltage = batV
-        self.pressure = pres
-        self.tempC = temp
-        self.humidity = hum
-        self.gpsLoc = gps
+    def __init__(self, data):
+        'data = (num, time, batV, pres, temp, hum, gps)'
+        self.packetnum = data[0]
+        self.timeSeconds = data[1]
+        self.batVoltage = data[2]
+        self.pressure = data[3]
+        self.tempC = data[4]
+        self.humidity = data[5]
+        self.gpsLoc = data[6]
     def returnType(self, i):
         '0:time,1:voltage,2:pressure,3:temperature,4:humidity'
         if i == 0:
@@ -33,15 +34,15 @@ class SensorData:
 
     def __init__(self):
         self.data = []
-    def addFrame(self, num, time, batV, pres, temp, hum, gps):
-        self.data.append(SensorFrame(num, time, batV, pres, temp, hum, gps))
+    def addFrame(self, frame):
+        self.data.append(frame)
     def graphData(self, i, j):
         '0:time,1:voltage,2:pressure,3:temperature,4:humidity'
         x = []
         y = []
-        for frame in self.data:
-            x.append(frame.returnType(i))
-            y.append(frame.returnType(j))
+        for img in self.data:
+            x.append(img.returnType(i))
+            y.append(img.returnType(j))
         return [x,y]
 
 class ImageFrame:
@@ -54,9 +55,11 @@ class ImageFrame:
         self.data = []
         self.data = [[[0 for x in range(self.colors)] for y in range(self.width)] for z in range(self.height)]
 
-    def addData(self, packetNum, listPixels):
-        'adds one packet of image data'
+    def addData(self, packet):
+        'adds one packet of image data --- packet = (packetNum, listPixels)'
         #70 pixels per packet
+        packetNum = packet[0]
+        listPixels = packet[1]
         xLoc = (packetNum*len(listPixels))%self.width
         yLoc = int((packetNum*len(listPixels))/self.width)
 
@@ -72,6 +75,9 @@ class ImageFrame:
             self.data[yPix][xPix] = listPixels[i]
         if packetNum == 8777:
             imd.addImage(self)
+            return True
+        else:
+            return False
         
 class ImageData:
     def __init__(self):
@@ -89,12 +95,12 @@ if __name__ == "__main__":
     imf = ImageFrame()
     imd = ImageData()
     sd = SensorData()
-    sd.addFrame(0, 1, 1, 1, 1, 1, [0,0,0])
-    sd.addFrame(0, 2, 1, 3, 2, 2, [0,0,0])
-    sd.addFrame(0, 3, 2, 3, 4, 3, [0,0,0])
+    sd.addFrame(SensorFrame((0, 1, 1, 1, 1, 1, [0,0,0])))
+    sd.addFrame(SensorFrame((0, 2, 1, 3, 2, 2, [0,0,0])))
+    sd.addFrame(SensorFrame((0, 3, 2, 3, 4, 3, [0,0,0])))
     print("image gen")
     for i in range(8778):
-        imf.addData(i, [[0,255,255] for j in range(35)])
+        imf.addData((i, [[0,255,255] for j in range(35)]))
     print("gen finished")
     implt = np.asarray(imd.getImage(0).data, dtype = np.uint8)
     plt.figure(1)
--- PacketParsing.py
+++ PacketParsing.py
@@ -1,5 +1,7 @@
 #Packet Parsing
 
+import Data
+
 def isTelemetry(packet):
     if (packet[0] < 64):
         print('is telemetry')
@@ -9,13 +11,24 @@ def isTelemetry(packet):
         #do image stuff to packet here
     print(packet[0])
         
-def telemetryDataAssembler(packet):
-    batV = packet[0]<<8 + packet[1]
-    print(batV)
-    print(packet[0])
-    print(packet[1])
+def telemetryDataAssembler(packet, num):
+    'converts telemetry packet to sensor frame'
+    batV = (packet[0]<<8) + packet[1]
+    time = (packet[2]<<8) + packet[3]
+    pres = packet[4] #this is not complete
+    altitude = packet[5] + packet[6] #this is not complete
+    temp = (packet[7]<<8) + packet[8]
+    hum = (packet[9]<<8) + packet[10]
+    gps = packet[11] #this is not complete
+    return (num, time, batV, pres, temp, hum, gps)
 
-def receivePacket(rawPacket):
+def imageDataAssembler(packet):
+    'converts image packet to image section tuple'
+    num = ((packet[0]&0b00111111)<<8) + packet[1]
+    return (num, packet[2:])
+    
+
+def convertPacket(rawPacket):
     'converts string packet to a list of 8-bit ints'
     packet = []
     for elt in rawPacket:
