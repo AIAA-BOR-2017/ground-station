b45c097a71a0fec93ec5c20e6dd256a227f3b21d
NathanSiegel <siegelnathan08@gmail.com>
2017-04-06 16:41:31 -0400

    improved some parsing, started xbee implementation

--- PacketParsing.py
+++ PacketParsing.py
@@ -3,11 +3,13 @@
 import Data
 
 def isTelemetry(packet):
-    if (packet[0] < 64):
+    if (~packet[0]&0b01000000):
         print('is telemetry')
+        return True
         #do telemetry stuff to packet here
     else:
         print('is image')
+        return False
         #do image stuff to packet here
     print(packet[0])
         
@@ -15,8 +17,8 @@ def telemetryDataAssembler(packet, num):
     'converts telemetry packet to sensor frame'
     batV = (packet[0]<<8) + packet[1]
     time = (packet[2]<<8) + packet[3]
-    pres = packet[4] #this is not complete
-    altitude = packet[5] + packet[6] #this is not complete
+    pres = (packet[4]<<4) + (packet[5]&0b11110000)
+    altitude = ((packet[5]&0b00001111)<<8) + packet[6]
     temp = (packet[7]<<8) + packet[8]
     hum = (packet[9]<<8) + packet[10]
     gps = packet[11] #this is not complete
--- Program.py
+++ Program.py
@@ -3,6 +3,7 @@
 
 import PacketParsing as pp
 import Data as da
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
@@ -12,8 +13,28 @@ sensordata = da.SensorData()
 curimg = da.ImageFrame()
 imagedata = da.ImageData()
 
+'''
+import serial
+import time
+from xbee import XBee
+
+
+serial_port = serial.Serial('/dev/ttyUSB0', 9600)
+xbee = ZigBee(serial_port, callback=print_data)
+
+def print_data(data):
+    """
+    This method is called whenever data is received
+    from the associated XBee device. Its first and
+    only argument is the data contained within the
+    frame.
+    """
+    print (data)
+'''
+
 if __name__ == "__main__":
     #do a thing
+    packet = ['a','a','a','a','a','a','a','a','a','a','a','a']
     if True: #packet received
         packet = pp.convertPacket(packet)
         if pp.isTelemetry(packet):
@@ -23,3 +44,8 @@ if __name__ == "__main__":
             if curimg.addData(pp.imageDataAssembler(packet)):
                 imagedata.addImage(curimg)
         
+
+'''
+xbee.halt()
+serial_port.close()
+'''
